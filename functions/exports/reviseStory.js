73529317cc39dbc8e7a0d75d250ef9ceca259ac4
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2021-06-12 05:26:05 +0000

    sanitize HTML, notably rm images on md render

--- functions/exports/reviseStory.js
+++ functions/exports/reviseStory.js
@@ -5,6 +5,7 @@ const { getRelatedStoryIDs } = require('../utils/related')
 
 const fetch = require('node-fetch')
 const { getAverageColor } = require('fast-average-color-node')
+const sanitizeHTML = require('sanitize-html')
 const marked = require('marked')
 
 exports.reviseStory = async ( change, { params: { storyID }, authType, auth } ) => {
@@ -23,7 +24,21 @@ exports.reviseStory = async ( change, { params: { storyID }, authType, auth } ) 
 	)
 
 	if (someIn(changes,'markdown'))
-		story.body = marked(after.markdown)
+		story.body = sanitizeHTML( marked(after.markdown), {
+			allowedTags: [
+				"address", "article", "aside", "footer", "header", "h1", "h2", "h3", "h4",
+				"h5", "h6", "hgroup", "main", "nav", "section", "blockquote", "dd", "div",
+				"dl", "dt", "figcaption", "figure", "hr", "li", "main", "ol", "p", "pre",
+				"ul", "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data", "dfn",
+				"em", "i", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp",
+				"small", "span", "strong", "sub", "sup", "time", "u", "var", "wbr", "caption",
+				"col", "colgroup", "table", "tbody", "td", "tfoot", "th", "thead", "tr"
+			  ],
+			  disallowedTagsMode: 'discard',
+			  allowedAttributes: {
+				a: [ 'href', 'name', 'target', 'title' ]
+			  },
+		})
 
 	if (someIn(changes,'timestamp'))
 		story.date = new Date(after.timestamp*1000)
