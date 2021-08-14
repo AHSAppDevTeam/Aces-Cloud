4b8e27e156204d89780e312e5394d4215c0fa30c
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2021-08-14 23:21:50 +0000

    Use new covid cases db structure

--- functions/exports/publishCases.js
+++ functions/exports/publishCases.js
@@ -5,7 +5,7 @@ const DOMParser = require('dom-parser')
 
 const parser = new DOMParser()
 
-exports.publishDashboard = async () => {
+exports.publishCases = async () => {
 	const rows = await fetch('https://www.ausd.net/apps/pages/ArcadiaUnifiedCOVID19Dashboard')
 	.then(res=>res.text())
 	.then(text=>parser.parseFromString(text))
@@ -19,11 +19,12 @@ exports.publishDashboard = async () => {
 	)
 	
 	const headers = rows[0].slice(1)
-	return Promise.all(rows.slice(1).map(row=>{
-		const school = row[0]
-		return Promise.all(row.slice(1).map((cell,index)=> {
-			const label = headers[index]
-			setDb(['covid-dashboard',school,label,Math.trunc(Date.now()/1000)],parseInt(cell))
-		}))
-	}))
+	return Promise.all(
+		rows.slice(1)
+		.map(row => row.slice(1)
+			.map( (cell,index) => [cell,row[0] + '-' + headers[index].split('-').pop()] )
+			.filter( ([cell,label]) => label.endsWith('-students') || label.endsWith('-staff') )
+			.map( ([cell,label]) => setDb(['covid-cases',label,Math.trunc(Date.now()/1000)],parseInt(cell)) )
+		).flat()
+	)
 }
