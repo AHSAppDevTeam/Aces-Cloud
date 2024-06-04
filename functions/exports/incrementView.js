da2df28613f817f4dd3c3e2a4e29975d25395893
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2024-06-03 17:09:11 -0700

    Update incrementView.js

--- functions/exports/incrementView.js
+++ functions/exports/incrementView.js
@@ -1,6 +1,7 @@
 const { getDb, setDb } = require('../utils/database')
 
 exports.incrementView = async ({ id }) => {
+	// checks if ID exists
 	if( !id || id.length > 64 || id.includes('/') ) return false
 	const snippet = await getDb( ['snippets',id] )
 	if( !snippet ) return false
